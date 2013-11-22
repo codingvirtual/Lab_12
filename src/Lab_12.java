956e1e44a7f279ca6c3940886fede0e8db9597a5
gspalen <kgpalen@att.net>
2013-11-22 14:45:41 -0600

    initial load

---
+++ src/Lab_12.java
@@ -0,0 +1,25 @@
+/**
+
+Create a GUI for this particular project. You can use the lab 4 solution that you’ve submitted.
+Your GUI is completely up to you and should display all information that you find useful. It 
+should be intuitive and informative.
+Create a program that keeps track of specific information for Students. The information stored 
+should be the following:
+First Name, Last Name, Major, GPA, UIN, NetID, Age, Gender,
+For this simple program we will only need to store 10 students in an ArrayList. Your students 
+should be stored in an object called Student.
+You should be able to add, sort (by any column you chose) and remove Students in the ArrayList.
+
+**/
+
+public class Lab_12 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
