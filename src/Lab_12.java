c4b82ace1b99fd484e18255730ff4c3ca5f9f7dc
gspalen <kgpalen@att.net>
2013-11-25 08:06:41 -0600

    project as submitted for grade

--- src/Lab_12.java
+++ src/Lab_12.java
@@ -1,7 +1,10 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Scanner;
+import java.util.*;
+import javax.swing.*;
+
+import java.awt.*;
+import javax.swing.border.*;
+import java.awt.Color;
+import java.awt.event.*;
 
 /**
 
@@ -17,171 +20,143 @@ You should be able to add, sort (by any column you chose) and remove Students in
 
 **/
 
-public class Lab_12 {
+
+
+public class Lab_12 extends JFrame implements ActionListener {
+	private JTextField txtMajor;
+	private JTextField txtGpa;
+	private JTextField txtUIN;
+	private JTextField txtNetID;
+	private JTextField txtAge;
+	private JTextField txtGender;
+	private Student_List roster = new Student_List();
+	private DefaultListModel displayList = new DefaultListModel();
+	private JList rosterList = new JList(displayList);
+	private JLabel lblIndex;
+
 
 	public static void main(String[] args) {
-		ArrayList<Student> roster = new ArrayList<Student>();
-		Scanner input = new Scanner(System.in);
-		 
-
-		System.out.println("Welcome to my student interface.");
-		System.out.println("Please select a number from the options below");
-		System.out.println("");
-		 
-
-		while (true) {
-		// Give the user a list of their options
-		System.out.println("1: Add a student to the roster.");
-		System.out.println("2: Remove a student from the roster.");
-		System.out.println("3: Sort the roster (and display).");
-		System.out.println("0: Exit the student interface.");
-		 
-
-		// Get the user input
-		int userChoice = input.nextInt();
-		 
-
-		switch (userChoice) {
-			case 1:		addStudent(roster);
-						break;
-			case 2:		removeStudent(roster);
-						break;
-			case 3:		sortRoster(roster);
-						break;
-			case 0:		System.out.println("Thank you for using the student interface. See you again soon!");
-						System.exit(0);
-			}
-		}
+		new Lab_12();
 	}
-		 
 
-	private static void sortRoster(ArrayList<Student> roster) {
-		Scanner input = new Scanner(System.in);
-		 
+	public Lab_12() {
+		this.setSize(800, 600);
+		setTitle("Student Roster List");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JPanel mainPanel = new JPanel();
+		mainPanel.setBorder(new TitledBorder(null, "Students", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		getContentPane().add(mainPanel, BorderLayout.CENTER);
+		mainPanel.setLayout(new BorderLayout(0, 0));
+		
+		JPanel rosterPanel = new JPanel();
+		mainPanel.add(rosterPanel, BorderLayout.CENTER);
+		rosterPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
 
-		System.out.println("Please select a field to sort by. All sorts will be 'ascending.'");
-		System.out.println("1 - First Name");
-		System.out.println("2 - Last Name");
-		System.out.println("3 - Major");
-		System.out.println("4 - GPA");
-		System.out.println("5 - UIN");
-		System.out.println("6 - NetID");
-		System.out.println("7 - Age");
-		System.out.println("8 - Gender");
-		System.out.print("Enter 1-8 now: ");
-		switch (input.nextInt()) {
-			// Did not include case 1 - choice 1 will be the default.
-			// If user enters any value other than 2-8, sort will be by first name by design.
-			case 2: 	Collections.sort(roster, new Comparator<Student>(){
+		rosterList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		rosterPanel.add(rosterList);
+		
+		JPanel headerPanel = new JPanel();
+		headerPanel.setBackground(new Color(255, 255, 255));
+		mainPanel.add(headerPanel, BorderLayout.NORTH);
+		headerPanel.setLayout(new GridLayout(0, 8, 0, 0));
+		
+		JButton nameField = new JButton("Name");
+		nameField.addActionListener(this);
+		
+		lblIndex = new JLabel("Index");
+		lblIndex.setHorizontalAlignment(SwingConstants.CENTER);
+		headerPanel.add(lblIndex);
+		nameField.setContentAreaFilled(false);
+		nameField.setDefaultCapable(false);
+		nameField.setToolTipText("Click to sort by name");
+		nameField.setBackground(new Color(255, 255, 255));
+		nameField.setBorder(new LineBorder(new Color(0, 0, 0)));
+		headerPanel.add(nameField);
+		
+		txtMajor = new JTextField();
+		txtMajor.setHorizontalAlignment(SwingConstants.CENTER);
+		txtMajor.setBorder(new LineBorder(new Color(171, 173, 179)));
+		txtMajor.setText("Major");
+		headerPanel.add(txtMajor);
+		txtMajor.setColumns(10);
+		
+		txtGpa = new JTextField();
+		txtGpa.setHorizontalAlignment(SwingConstants.CENTER);
+		txtGpa.setText("GPA");
+		headerPanel.add(txtGpa);
+		txtGpa.setColumns(10);
+		
+		txtUIN = new JTextField();
+		txtUIN.setHorizontalAlignment(SwingConstants.CENTER);
+		txtUIN.setText("UIN");
+		headerPanel.add(txtUIN);
+		txtUIN.setColumns(10);
+		
+		txtNetID = new JTextField();
+		txtNetID.setHorizontalAlignment(SwingConstants.CENTER);
+		txtNetID.setText("NetID");
+		headerPanel.add(txtNetID);
+		txtNetID.setColumns(10);
+		
+		txtAge = new JTextField();
+		txtAge.setHorizontalAlignment(SwingConstants.CENTER);
+		txtAge.setText("Age");
+		headerPanel.add(txtAge);
+		txtAge.setColumns(10);
+		
+		txtGender = new JTextField();
+		txtGender.setText("Gender");
+		txtGender.setHorizontalAlignment(SwingConstants.CENTER);
+		headerPanel.add(txtGender);
+		txtGender.setColumns(10);
+		
+		JButton addStudent = new JButton("+");
+		addStudent.addActionListener(this);
+		
+		getContentPane().add(addStudent, BorderLayout.WEST);
+		
+		JButton removeStudent = new JButton("-");
+		removeStudent.addActionListener(this);
+		getContentPane().add(removeStudent, BorderLayout.EAST);
+		this.setVisible(true);
+		
+		updateList();
+	}
+
+
+private void updateList() {
+	displayList.clear();
+	for (Student s : roster) {
+		String[] parsedList = s.toString().split("[|]");
+		displayList.addElement(String.format("%-30s%5s, %5s%-25s%-25s%-25s%-25s%-25s%-25s", parsedList[0], parsedList[2], parsedList[1], parsedList[3], parsedList[4], parsedList[5], parsedList[6], parsedList[7], parsedList[8]));
+	}
+}
+
+	private static void sortRoster(Student_List roster) {
+		Collections.sort(roster, new Comparator<Student>(){
 							public int compare(Student s1, Student s2) {
 								return s1.getLast().compareToIgnoreCase(s2.getLast());
 							}
 						});
-						break;
-			case 3: 	Collections.sort(roster, new Comparator<Student>(){
-							public int compare(Student s1, Student s2) {
-								return s1.getMajor().compareToIgnoreCase(s2.getMajor());
-							}
-						});
-						break;
-			case 4: 	Collections.sort(roster, new Comparator<Student>(){
-							public int compare(Student s1, Student s2) {
-								return (int) (s1.getGPA() - s2.getGPA());
-							}
-						});
-						break;
-			case 5: 	Collections.sort(roster, new Comparator<Student>(){
-							public int compare(Student s1, Student s2) {
-								return s1.getUIN() - s2.getUIN();
-							}
-						});
-						break;
-			case 6: 	Collections.sort(roster, new Comparator<Student>(){
-							public int compare(Student s1, Student s2) {
-								return s1.getNetID().compareToIgnoreCase(s2.getNetID());
-							}
-						});
-						break;
-			case 7: 	Collections.sort(roster, new Comparator<Student>(){
-							public int compare(Student s1, Student s2) {
-								return s1.getAge() - s2.getAge();
-							}
-						});
-						break;
-			case 8: 	Collections.sort(roster, new Comparator<Student>(){
-							public int compare(Student s1, Student s2) {
-								return s1.getGender().compareToIgnoreCase(s2.getGender());
-							}
-						});
-						break;
-			default:	Collections.sort(roster, new Comparator<Student>(){
-							public int compare(Student s1, Student s2) {
-								return s1.getFirst().compareToIgnoreCase(s2.getFirst());
-							}
-						});
-						break;
-			}
-		 
-
-		for (Student student : roster) {
-			System.out.println(student.toString());
-		}
 	}
 		 
+	
 
-	private static void removeStudent(ArrayList<Student> roster) {
-		Scanner input = new Scanner(System.in);
-		String fname, lname;
-		Boolean removed = false;
-		 
-
-		System.out.print("Enter first name of student to remove: ");
-		fname = input.next();
-		System.out.print("Enter last name of student to remove: ");
-		lname = input.next();
-		for (Student student : roster) {
-			if (fname.equals(student.getFirst()) && lname.equals(student.getLast())) {
-				roster.remove(student);
-				removed = true;
-			}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		switch (e.getActionCommand()) {
+			case "+":		new Student_Entry_Dialog(roster);
+							break;
+			case "-":		roster.removeStudent(Integer.parseInt(rosterList.getSelectedValue().toString().substring(0, 29).trim()));
+							break;
+			case "Name":	sortRoster(roster);
+							break;
 		}
-		 
-
-		if (removed) {
-			System.out.printf("All records for %s %s have been removed.\n", fname, lname);
-		}
-		else {
-			System.out.printf("There were no matches found for %s %s.\n", fname, lname);
-		}
-	}
-		 
-
-	private static void addStudent(ArrayList<Student> roster) {
-		String first, last, major, NetID, gender;
-		Double GPA;
-		int UIN, age;
-		Scanner input = new Scanner(System.in);
-		 
-
-		System.out.println("Add student to roster.");
-		System.out.print("First name: ");
-		first = input.next();
-		System.out.print("Last name: ");
-		last = input.next();
-		System.out.print("Major: ");
-		major = input.next();
-		System.out.print("GPA (format: X.XX): ");
-		GPA = input.nextDouble();
-		System.out.print("UIN: ");
-		UIN = input.nextInt();
-		System.out.print("NetID: ");
-		NetID = input.next();
-		System.out.print("Age: ");
-		age = input.nextInt();
-		System.out.print("Gender: ");
-		gender = input.next();
-		roster.add(new Student(first, last, major, GPA, UIN, NetID, age, gender));
-		System.out.printf("%s %s has been added to the roster\n", first, last);
+		
+		updateList();
 	}
 }
 
